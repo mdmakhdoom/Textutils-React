23be5f0a9c256629dcbd617cf42a5374ac42487b
Mak <makhdoomansari45@gmail.com>
2023-03-04 01:42:09 +0530

    Fixing issues and wrapping

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import './App.css';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-import React, { useState } from 'react';
-
-
+ 
 function App() {
-  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [mode, setMode] = useState('light'); 
   const [alert, setAlert] = useState(null);
-
   const showAlert = (message, type)=>{
       setAlert({
         msg: message,
@@ -18,33 +15,22 @@ function App() {
           setAlert(null);
       }, 1500);
   }
-
   const toggleMode = ()=>{
     if(mode === 'light'){
       setMode('dark');
-      document.body.style.backgroundColor = '#042743';
-      showAlert("Dark mode has been enabled", "success");
-      document.title = 'TextUtils - Dark Mode';
-      // setInterval(() => {
-      //   document.title = 'TextUtils is Amazing Mode';
-      // }, 2000);
-      // setInterval(() => {
-      //   document.title = 'Install TextUtils Now';
-      // }, 1500);
+      document.body.style.backgroundColor = '#343a40';
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
-      showAlert("Light mode has been enabled", "success");
-      document.title = 'TextUtils - Light Mode';
     }
   }
   return (
     <>
-    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
     <Alert alert={alert}/>
     <div className="container my-3">
-    <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
+    <TextForm showAlert={showAlert} heading="Enter your text to analyze below" mode={mode}/>
     </div>
     </> 
   );
